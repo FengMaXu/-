2119f240ec9564aec36170f3ddc4c1db4438af45
fmax <fnngmax@gmail.com>
2026-02-03 11:06:34 +0800

    Cleanup redundant test and debug scripts

--- debug_import.py
+++
@@ -1,9 +0,0 @@
-try:
-    print("Attempting to import app.api.server...")
-    from app.api.server import app
-
-    print("Import successful!")
-except Exception as e:
-    import traceback
-
-    traceback.print_exc()
